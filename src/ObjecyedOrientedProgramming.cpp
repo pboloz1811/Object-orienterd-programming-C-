defbf3207c8263930b6c8a7c032e7064872c5849
Patryk <patryk@MacBook-Pro-Patryk.local>
2017-02-17 15:02:17 +0100

    shape factory

--- src/ObjecyedOrientedProgramming.cpp
+++ src/ObjecyedOrientedProgramming.cpp
@@ -12,8 +12,46 @@
 using namespace std;
 
 
+class Shape {
+public:
+	virtual void rysuj()=0;
+};
 
+class Triangle: public Shape{
+public:
+	void rysuj(){
+		cout << "  *  " << "\n" << " *** " << "\n" << "*****";
+	}
+};
+
+class Square: public Shape {
+public:
+	void rysuj(){
+		cout << "*****" << "\n" << "*****" << "\n" << "*****";
+	}
+};
+
+enum type {
+	Tri, Squ
+};
+
+class ShapeFactory {
+public:
+	Shape *createShape(type typ){
+		switch (typ) {
+		case Tri:
+			return new Triangle();
+			break;
+		case Squ:
+			return new Square();
+			break;
+		}
+	}
+};
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	ShapeFactory *shapeFactory = new ShapeFactory();
+	Shape *triangle = shapeFactory ->createShape(Squ);
+	triangle -> rysuj();
 }
